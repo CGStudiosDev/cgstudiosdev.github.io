9709d2158255b81d564b5b59b0b8e734f5cef4ed
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2021-01-16 21:29:36 +0000

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+function switchimg() {
+    document.getElementById("logoimg").src = "images/cgstudiosalpha.png"
+}
+
+function switchimgback() {
+    document.getElementById("logoimg").src = "images/cgstudiosalphadark.png"
+}
+
+function home() {
+    window.location.assign("/")
+}
